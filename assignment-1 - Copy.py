bd05c3a8a2c30c0457efe31996626a67bc8b594d
harshup4 <harshup4@gmail.com>
2026-02-10 12:00:33 +0530

    added copy

---
+++ assignment-1 - Copy.py
@@ -0,0 +1,12 @@
+print("----Assignment-1-----")
+num = 5
+for x in range(1,11):
+    result = num*x
+    print(num ,'*' ,x ,'is: ' ,result)
+
+print("----Assignment-2-----")
+for i in range(1,21):
+    if i%2==0:
+        print("Even Number: ", i)
+    else:
+        print("Odd Number: ", i)
